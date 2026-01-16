43cc4667b1def248d7efb0a41baa98b290efe5e6
Azka Syakirah <azkasyakir800@gmail.comA>
2026-01-16 14:20:13 +0700

    creating product card components

---
+++ lib/utils/currency_format.dart
@@ -0,0 +1,11 @@
+import 'package:intl/intl.dart';
+
+String formatRupiah(int number) {
+  final currencyFormatter = NumberFormat.currency(
+    locale: 'id_ID',
+    symbol: 'Rp. ',
+    decimalDigits: 0,
+  );
+  
+  return currencyFormatter.format(number);
+}
